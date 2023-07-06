1ca70f7a3acc1a9c903cd282b743f91e3b56b058
OnpointSoftwares <vincentbettoh@gmail.com>
2023-07-06 14:17:50 -0400

    CHANGES

--- db/billing.sql
+++ db/billing.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 13, 2023 at 01:20 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: localhost
+-- Generation Time: Jul 06, 2023 at 08:16 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,36 +18,28 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `vms`
+-- Database: `billing`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookings`
+-- Table structure for table `billings`
 --
 
-CREATE TABLE `bookings` (
+CREATE TABLE `billings` (
   `id` int(11) NOT NULL,
-  `VisitorName` varchar(30) NOT NULL,
+  `CompanyName` varchar(30) NOT NULL,
   `MobileNo` int(11) NOT NULL,
   `EmailAddress` varchar(30) NOT NULL,
   `Date` date NOT NULL,
-  `Office` varchar(30) NOT NULL,
+  `UserEmail` varchar(30) NOT NULL,
   `status` varchar(30) NOT NULL DEFAULT 'waiting',
   `user` varchar(50) NOT NULL,
-  `approval` int(11) NOT NULL DEFAULT '0',
-  `Image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `bookings`
---
-
-INSERT INTO `bookings` (`id`, `VisitorName`, `MobileNo`, `EmailAddress`, `Date`, `Office`, `status`, `user`, `approval`, `Image`) VALUES
-(7, 'VINCENT KIPKURUI', 702502952, 'vincentbettoh@gmail.com', '2023-03-28', 'Post graduate', 'confirmed', 'vincentbettoh@kabarak.ac.ke', 1, ''),
-(10, 'VINCENT KIPKURUI', 702502952, 'vincentbettoh@gmail.com', '2023-03-07', 'Kabarak enterprises', 'confirmed', 'vincentbettoh@kabarak.ac.ke', 1, ''),
-(11, 'purity', 725444604, 'puritycherono@kabarak.ac.ke', '2023-03-13', 'Finance', 'confirmed', 'puritycherono@kabarak.ac.ke', 1, '');
+  `AmountSpent` int(11) NOT NULL DEFAULT 0,
+  `TotalTax` int(100) NOT NULL,
+  `TotalAmount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -63,27 +56,20 @@ CREATE TABLE `users` (
   `username` varchar(30) NOT NULL,
   `password` varchar(10) NOT NULL,
   `role` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`Firstname`, `Lastname`, `IDNO`, `Phone`, `id`, `username`, `password`, `role`) VALUES
-('Purity', 'Cherono', 38075877, '0725444604', 1, 'admin', '123456', 'admin'),
+('Admin', 'Admin', 38075877, '0725444604', 1, 'admin', '123456', 'admin'),
 ('purity', 'cherono', 38075877, '0725444604', 3, 'receptionist', '123456', 'receptionist'),
-('purity', 'cherono', 38075877, '072544604', 7, 'puritycherono@kabarak.ac.ke', '123456', 'visitor');
+('purity', 'cherono', 38075877, '072544604', 7, 'puritycherono@kabarak.ac.ke', '123456', 'user');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `bookings`
---
-ALTER TABLE `bookings`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `unique` (`id`);
 
 --
 -- Indexes for table `users`
@@ -96,15 +82,12 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `bookings`
---
-ALTER TABLE `bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
