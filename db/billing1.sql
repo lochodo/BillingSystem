f454268b0f063869d967c0685c8c621ff6a09794
unknown <vincentbettoh@gmail.com>
2023-07-13 15:51:21 +0300

    Changes

--- db/billing1.sql
+++ db/billing1.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jul 06, 2023 at 08:16 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.1.17
+-- Host: 127.0.0.1
+-- Generation Time: Jul 13, 2023 at 02:49 PM
+-- Server version: 10.1.40-MariaDB
+-- PHP Version: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,16 +31,24 @@ SET time_zone = "+00:00";
 CREATE TABLE `billings` (
   `id` int(11) NOT NULL,
   `CompanyName` varchar(30) NOT NULL,
-  `MobileNo` int(11) NOT NULL,
-  `EmailAddress` varchar(30) NOT NULL,
-  `Date` date NOT NULL,
-  `UserEmail` varchar(30) NOT NULL,
-  `status` varchar(30) NOT NULL DEFAULT 'waiting',
-  `user` varchar(50) NOT NULL,
-  `AmountSpent` int(11) NOT NULL DEFAULT 0,
-  `TotalTax` int(100) NOT NULL,
+  `Date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(30) NOT NULL DEFAULT 'unpaid',
+  `AmountWaterSpent` int(11) NOT NULL DEFAULT '0',
+  `TotalWaterax` int(100) NOT NULL,
+  `TotalWaterAmount` int(11) NOT NULL,
+  `ElectricityAmountSpent` int(100) NOT NULL,
+  `ElectricityTax` int(100) NOT NULL,
+  `TotalAmountElectricity` int(100) NOT NULL,
   `TotalAmount` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `billings`
+--
+
+INSERT INTO `billings` (`id`, `CompanyName`, `Date`, `status`, `AmountWaterSpent`, `TotalWaterax`, `TotalWaterAmount`, `ElectricityAmountSpent`, `ElectricityTax`, `TotalAmountElectricity`, `TotalAmount`) VALUES
+(16, 'purity', '2023-07-13 13:56:44', 'unpaid', 3000, 300, 3300, 3000, 500, 5500, 8800),
+(17, 'purity', '2023-07-13 15:21:24', 'unpaid', 4000, 400, 4400, 50000, 5000, 55000, 59900);
 
 -- --------------------------------------------------------
 
@@ -56,7 +65,7 @@ CREATE TABLE `users` (
   `username` varchar(30) NOT NULL,
   `password` varchar(10) NOT NULL,
   `role` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -64,12 +73,17 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`Firstname`, `Lastname`, `IDNO`, `Phone`, `id`, `username`, `password`, `role`) VALUES
 ('Admin', 'Admin', 38075877, '0725444604', 1, 'admin', '123456', 'admin'),
-('purity', 'cherono', 38075877, '0725444604', 3, 'receptionist', '123456', 'receptionist'),
 ('purity', 'cherono', 38075877, '072544604', 7, 'puritycherono@kabarak.ac.ke', '123456', 'user');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `billings`
+--
+ALTER TABLE `billings`
+  ADD UNIQUE KEY `id` (`id`,`CompanyName`);
 
 --
 -- Indexes for table `users`
@@ -80,6 +94,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `billings`
+--
+ALTER TABLE `billings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
